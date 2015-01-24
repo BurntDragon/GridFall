690f3a410731c95ec5f0ab481b986c46768d9e61
tashows <tashows@516805f8-b5ab-4f7d-8d84-62e92a1dcc46>
2015-01-24 17:30:43 +0000

    Audio files and part of Sound manager skatogrammeno O:)

--- Assets/Scripts/ctrl/SoundManager.cs
+++ Assets/Scripts/ctrl/SoundManager.cs
@@ -1,15 +1,80 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class SoundManager : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
+[RequireComponent (typeof (AudioSource))]
+public class CtrlPlayerSoundManager : MonoBehaviour 
+{
+	public AudioClip[] footsteps;
+	public AudioClip tileUp;
+	public AudioClip tileWarn;
+	public AudioClip tileDown;
+	//public AudioClip ;
+	//public AudioClip ;
+	//public AudioClip ;
 	
+	//public AudioClip[] deposit;
+	
+//	void OnEnable()
+//	{
+//		EventManager.stuffPickup += playCollect;
+//		EventManager.gotStunned += playBirds;
+//		EventManager.stunnEnemy += playRangedStun;
+//		EventManager.stunnEnemy += playLaugh;
+//	}
+//	
+//	void OnDisable()
+//	{
+//		EventManager.stuffPickup -= playCollect;
+//		EventManager.gotStunned -= playBirds;
+//		EventManager.stunnEnemy -= playRangedStun;
+//		EventManager.stunnEnemy -= playLaugh;
+//	}
+	
+//	void playRangedStun(string enemy)
+//	{
+//		GetComponent<AudioSource>().PlayOneShot(closeStun);
+//	}
+	
+	void playTileUp()
+	{
+		GetComponent<AudioSource>().PlayOneShot(tileUp);
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
+	void playTileWarn()
+	{
+		GetComponent<AudioSource>().PlayOneShot(tileWarn);
 	}
+	
+	void playTileDown()
+	{
+		GetComponent<AudioSource>().PlayOneShot(tileDown);
+	}
+	
+//	public void playFootsteps()
+//	{
+//		GetComponent<AudioSource>().PlayOneShot(footsteps[Random.Range(0, (footsteps.Length-1))]);
+//	}
+//	
+//	void playBirds(string enemy)
+//	{
+//		GetComponent<AudioSource>().PlayOneShot(birds);
+//	}
+//	
+//	void playLaugh(string enemy)
+//	{
+//		GetComponent<AudioSource>().PlayOneShot(laugh[Random.Range(0, laugh.Length-1)]);
+//	}
+//	
+//	public void playDeposit(int sound)
+//	{
+//		sound--;
+//		
+//		if (sound>5)
+//		{
+//			sound=5;
+//		}
+//		
+//		GetComponent<AudioSource>().PlayOneShot(deposit[sound]);
+//	}
+	
 }
